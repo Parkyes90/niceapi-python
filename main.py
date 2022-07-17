4030cec00a44f96f96f464d0efe6fc831e661a79
parkyes90 <parkyes90@gmail.com>
2022-07-18 08:17:04 +0900

    add envs

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 
+from base import (
+    NICE_API_CLIENT_SECRET,
+    NICE_API_IPIN_PRODUCT_CODE,
+    NICE_API_IV_INTEGRATED_PRODUCT_CODE,
+    NICE_API_CLIENT_ID,
+)
 from crypto_token import get_crypto_token, get_request_crypto_token_data
 from oauth_token import get_oauth_token
 from symmetric_key import get_symmetric_key
@@ -18,10 +24,14 @@ async def say_hello(name: str):
 
 
 if __name__ == "__main__":
-    oauth_token = get_oauth_token()
-    request_data = get_request_crypto_token_data()
-    if isinstance(oauth_token.dataBody, str):
-        crypto_token = get_crypto_token("dummy", request_data)
-    else:
-        crypto_token = get_crypto_token(oauth_token.dataBody.access_token, request_data)
-    symmetric_key = get_symmetric_key(request_data, crypto_token)
+    # oauth_token = get_oauth_token()
+    # request_data = get_request_crypto_token_data()
+    # if isinstance(oauth_token.dataBody, str):
+    #     crypto_token = get_crypto_token("dummy", request_data)
+    # else:
+    #     crypto_token = get_crypto_token(oauth_token.dataBody.access_token, request_data)
+    # symmetric_key = get_symmetric_key(request_data, crypto_token)
+    print(NICE_API_IPIN_PRODUCT_CODE)
+    print(NICE_API_IV_INTEGRATED_PRODUCT_CODE)
+    print(NICE_API_CLIENT_ID)
+    print(NICE_API_CLIENT_SECRET)
